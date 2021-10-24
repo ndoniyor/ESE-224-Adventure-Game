3a68e79b771a6afbb1192cbc9f89bee6d4e545ba
NehalKabir <92537145+NehalKabir@users.noreply.github.com>
2021-10-23 21:09:31 -0400

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,138 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class player
+{
+private:
+    string Name;            //This should hold the player’s name
+    int MaxHealth;          //This should hold the maximum health of the player
+    int HealthPoints;       //This should hold the current health of the player
+    int AttackDamage;       //This should hold the attack damage of the player
+public:
+    player(string n, int mh, int hp, int ad);               //default constructor
+    string getName();
+    int getMaxHP();         //returns the maximum health
+    int getHP();            //returns the current health
+    int getDMG();           //returns the current damage
+    void setName(string n);
+    void setMaxHP(int mh);
+    void setHP(int hp);        //sets the current health
+    void setDMG(int ad);       //sets the current damage
+    void ModifyHealth(int x); //Increments or decrements the player’s health by the input value
+    void ModifyDamage(int x); //Increments or decrements the player’s attack damage by the input value
+
+    friend ostream& operator <<(ostream& os, const player& p); //needs to be implemented
+};
+
+
+
+class enemy
+{
+private:
+    string Name;            //This should hold the player’s name
+    int MaxHealth;          //This should hold the maximum health of the player
+    int HealthPoints;       //This should hold the current health of the player
+    int AttackDamage;       //This should hold the attack damage of the player
+    int Level = 0;
+public:
+    enemy(string n, int mh, int hp, int ad, int lvl);  //default constructor
+    string getName();
+    int getMaxHP();                 //returns the maximum health
+    int getHP();                    //returns the current health
+    int getDMG();                   //returns the current damage
+    void setName(string n);
+    void setMaxHP(int mh);
+    void setHP(int hp);             //sets the current health
+    void setDMG(int ad);            //sets the current damage
+    void ModifyHealth(int x);       //Increments or decrements the player’s health by the input value
+    void ModifyDamage(int x);   //Increments or decrements the player’s attack damage by the input value
+
+    friend ostream& operator <<(ostream& os, const enemy& p); //needs to be implemented
+};
+
+
+
+class weapon
+{
+private:
+    string W_item;
+    int W_damage;
+    int W_uses;
+    int W_cost;
+public:
+    weapon();
+    weapon(string i, int d);
+    void setItem(string);
+    void setDMG(int);
+    void setUses(int);
+    void setCost(int);
+    string getItem();
+    int getDMG();
+    int getUses();
+    int getCost();
+    bool degradation();
+};
+
+
+class scoreboard
+{
+private:
+    string SB_Name;            //This should hold the player’s name
+    int SB_score;              // score of player
+    string SB_difficulty;       //dif of game
+    int SB_difficultyMod;
+    int SB_Floor;
+public:
+    scoreboard();
+    scoreboard(string n, int s, string d, int dm, int f);
+    void setName(string n);
+    void setScore(int s);
+    void setDiff(string d);
+    void setDiffMod(int dm);
+    void setFloor(int f);
+
+    string getName();
+    int getScore();
+    string getDiff();
+    int getFloor();
+
+    void AddScore(int s);
+
+    void SB_out();
+
+    friend ostream& operator <<(ostream& os, const scoreboard& p); //needs to be implemented
+};
+
+class FileOperations {
+private:
+    string SB_file_name = "scoreboard.txt";
+    //holds the name of the scoredboard file
+    fstream SB_myFile;
+    //opens the scoreboard file
+
+    string P_file_name;
+    //holds the name of the player file
+    fstream p_myFile;
+    //opens the player file
+    vector<int> sortedScores;
+    vector<string> sortedNames;
+
+
+
+public:
+    void Save2FileP(player p);
+    //function Saves player progress into a file
+    void Save2FileS(scoreboard s);
+    //function saves scoreboard progress into a file
+    void ChooseFile(scoreboard&, player&);
+    //function the asks the user to select the file to use
+    void CloseFile();
+    //function to close the files
+    void ScoreRank();
+    //function to output scores from all the scoreboards
+};
---
+++ fileoperations.cpp
@@ -0,0 +1,82 @@
+#include "Header.h"
+#include "scoreboard.h"
+
+
+void FileOperations::Save2FileP(player p) {
+	cout << "Which file would you like to save to?";
+	cin >> P_file_name;
+	p_myFile.open(P_file_name);
+	if (p_myFile.fail()) {
+		cerr << "Player file could not be opened";
+		exit(1);
+	}
+
+	p_myFile << p.getMaxHP() << "\n";
+	p_myFile << p.getHP() << "\n";
+	p_myFile << p.getDMG() << "\n";
+}
+
+void FileOperations::Save2FileS(scoreboard s) {
+
+	SB_myFile.open(SB_file_name);
+	if (SB_myFile.fail()) {
+		cerr << "Scoreboard file could not be opened";
+		exit(1);
+	}
+
+	SB_myFile << "Name: " << s.getName() << "\n";
+	SB_myFile << "Score: " << s.getScore() << "\n";
+	SB_myFile << s.getDiff() << "\n";
+	SB_myFile << s.getDiff() << "\n";
+	SB_myFile << s.getFloor() << "\n";
+
+	if (sortedScores.size() == 0 && sortedNames.size() == 0) {
+		sortedScores.push_back(s.getScore());
+		sortedNames.push_back(s.getName());
+	}
+	else {
+		for (int i = 0; i < sortedScores.size(); i++) {
+			if (s.getScore() < sortedScores[i]) {
+				;
+			}
+			else {
+				sortedScores.insert(sortedScores.begin() + i, s.getScore());
+				sortedNames.insert(sortedNames.begin() + i, s.getName());
+			}
+		}
+	}
+
+}
+
+void FileOperations::ChooseFile(scoreboard&, player&) {
+	char oper;
+	string name;
+	fstream file1;
+	cout << "Would you like to create a new file (n) or open an existing one (e)." << endl;
+	cin >> oper;
+	if (oper == 'n') {
+		cout << "What would you like to call this file?";
+		cin >> name;
+		file1.open(name);
+		cout << "New file successfully created.";
+	}
+	else if (oper == 'e') {
+		cout << "Which existing file would you like to choose?";
+		cin >> name;
+		file1.open(name);
+		if (file1.fail()) {
+			cerr << "This file does not exist.";
+		}
+	}
+}
+void FileOperations::CloseFile() {
+	SB_myFile.close();
+	p_myFile.close();
+}
+
+void FileOperations::ScoreRank() {
+	for (int i = 0; i < sortedScores.size(); i++) {
+		cout << "Player: " << sortedNames[i] << endl;
+		cout << "Score: " << sortedScores[i] << endl;
+	}
+}
